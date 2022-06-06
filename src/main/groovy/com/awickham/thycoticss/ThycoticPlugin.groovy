ad8e36aea4f770c237654dc43af7c4db08f51c25
Wickham.Andrew.J <AndrewWickham@westfieldgrp.com>
2022-06-06 11:18:14 -0400

    Tabs to spaces

--- src/main/groovy/com/awickham/thycoticss/ThycoticPlugin.groovy
+++ src/main/groovy/com/awickham/thycoticss/ThycoticPlugin.groovy
@@ -25,24 +25,24 @@ import com.morpheusdata.core.Plugin
  */
 class ThycoticPlugin extends Plugin {
 
-	@Override
-	String getCode() {
-		return 'awickham-thycoticss-plugin'
-	}
+    @Override
+    String getCode() {
+        return 'awickham-thycoticss-plugin'
+    }
 
-	@Override
-	void initialize() {
-		SecretServerCredentialProvider secretServerCredentialProvider = new SecretServerCredentialProvider(this, morpheus)
-		this.pluginProviders.put("thycoticss", secretServerCredentialProvider)
-		this.pluginProviders.put("thycoticss-cypher", new SecretServerCypherProvider(this,morpheus))
-		this.setName("Thycotic Secret Server")
-	}
+    @Override
+    void initialize() {
+        SecretServerCredentialProvider secretServerCredentialProvider = new SecretServerCredentialProvider(this, morpheus)
+        this.pluginProviders.put("thycoticss", secretServerCredentialProvider)
+        this.pluginProviders.put("thycoticss-cypher", new SecretServerCypherProvider(this,morpheus))
+        this.setName("Thycotic Secret Server")
+    }
 
-	/**
-	 * Called when a plugin is being removed from the plugin manager (aka Uninstalled)
-	 */
-	@Override
-	void onDestroy() {
-		//nothing to do for now
-	}
+    /**
+     * Called when a plugin is being removed from the plugin manager (aka Uninstalled)
+     */
+    @Override
+    void onDestroy() {
+        //nothing to do for now
+    }
 }
